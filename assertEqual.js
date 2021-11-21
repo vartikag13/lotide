51d04bb626ed1ea591ee6916b9759adfa1fca6c5
Vartika Gupta <vartika.gupta13@gmail.com>
2021-11-21 02:18:32 +0000

    modularized assertEqual.js and head.js, added test files for both of them

--- assertEqual.js
+++ assertEqual.js
@@ -7,6 +7,4 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✔ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} != ${expected}`);
-  }
-};
-
 const head = (givenList) => {
   return givenList[0];
 
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
