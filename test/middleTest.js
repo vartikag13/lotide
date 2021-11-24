731649564ffa4c33fdf7b741afe695f9f75bc34f
Vartika Gupta <vartika.gupta13@gmail.com>
2021-11-24 14:05:31 -0500

    refactored middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -5,18 +5,18 @@ const middle = require('../middle');
 //TEST ASSERTIONs
 describe("#middle", () => {
   it("returns [] for [1]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1]), []), true);
+    assert.deepEqual(middle([1]), []);
   });
     
   it("returns [] for [1, 2]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2]), []), true);
+    assert.deepEqual(middle([1, 2]), []);
   });
       
   it("returns [2] for [1, 2, 3]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3]), [2]), true);
+    assert.deepEqual(middle([1, 2, 3]), [2]);
   });
     
   it("returns [2, 3] for [1, 2, 3]", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]), true);
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 });
